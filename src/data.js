133ba652ad0b4f2a43f2ee86e3e6f07baf32567d
Prashant Bhardwaj <geekyprashant840@gmail.com>
2023-04-06 16:53:49 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const questions = [
+  {
+    id: 1,
+    title: "What is JSX?",
+    info: "JSX is a syntax extension of JavaScript. It is used with React to describe what the user interface should look like. By using JSX, we can write HTML structures in the same file that contains JavaScript code.",
+  },
+  {
+    id: 2,
+    title: "What is the virtual DOM?",
+    info: "DOM stands for Document Object Model. The DOM represents an HTML document with a logical tree structure. Each branch of the tree ends in a node, and each node contains objects.",
+  },
+  {
+    id: 3,
+    title: "What are synthetic events in React?",
+    info: "Synthetic events combine the response of different browser's native events into one API, ensuring that the events are consistent across different browsers.",
+  },
+  {
+    id: 4,
+    title: "Why is there a need for using keys in Lists?",
+    info: "A key is a unique identifier and it is used to identify which items have changed, been updated or deleted from the lists.",
+  },
+  {
+    id: 5,
+    title: "What are the components in React?",
+    info: "Components are the building blocks of any React application, and a single app usually consists of multiple components. A component is essentially a piece of the user interface. It splits the user interface into independent, reusable parts that can be processed separately.",
+  },
+];
+export default questions;
